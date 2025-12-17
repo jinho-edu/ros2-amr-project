a795d2f5dc72c2f00a0db78f922a4ad608512425
jinho-edu <96398731+jinho-edu@users.noreply.github.com>
2025-12-17 16:20:19 +0900

    Update sim_headless.launch.py - original nodes

--- src/amr_nav/launch/sim_headless.launch.py
+++ src/amr_nav/launch/sim_headless.launch.py
@@ -3,9 +3,12 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(
-            package="amr_nav",
-            executable="dummy_node",
-            output="screen",
-        )
+        # Node(
+        #     package="amr_nav",
+        #     executable="dummy_node",
+        #     output="screen",
+        # )
+        Node(package='amr_nav', executable='simple_controller'),
+        Node(package='amr_nav', executable='node_health'),
     ])
+
